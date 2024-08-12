f8c6120ebb7968843c36a725d812ba481340bd40
Raydel Piloto Quesada <pilotoraydel@gmail.com>
2024-08-12 16:50:44 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Raydelpq\WhatsappApi\Http\Controllers\WhatsAppController;
 
 Route::post('/api/whatsapp',  [WhatsAppController::class, 'loadTaxistas'])->name('whatsapp_load');
+Route::post('/api/whatsapp/registro',  [WhatsAppController::class, 'registroTaxista'])->name('whatsapp_registro');
 Route::post('/api/expulsar',  [WhatsAppController::class, 'expulsarTaxistas'])->name('whatsapp_expulsar');
 Route::post('/api/getFondo',  [WhatsAppController::class, 'getFondo'])->name('whatsapp_getFondo');
