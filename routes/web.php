3e0da12256bb9c5ef4be8d1c8ade9495eac3cd83
Raydel <92547407+Raydelpq@users.noreply.github.com>
2024-12-02 11:26:27 -0500

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Raydelpq\WhatsappApi\Http\Livewire\Link;
 use Raydelpq\WhatsappApi\Http\Livewire\GetQr;
 
 Route::get('/getqr',GetQr::class)->name("getqr");
+Route::get('/apiwhatsapp',Link::class)->name("config_api");
