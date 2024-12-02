eb466a1fbfb2cc9844895d50a860014fc6ff6d96
Raydel <92547407+Raydelpq@users.noreply.github.com>
2024-12-02 11:26:48 -0500

    Add files via upload

--- config/whatsappapi.php
+++ config/whatsappapi.php
@@ -7,5 +7,6 @@ return [
     'PORT_WA' => env('PORT_WA') ? env('PORT_WA') : '99000',
     'ENDPOINT_WA' => env('ENDPOINT_WA') ? env('ENDPOINT_WA') : '/api/v1',
     'CANAL_WA' => env('CANAL_WA') ? env('CANAL_WA') : 'setQR_',
-    'Pusher_WA' => env('PUSHER_APP_KEY') ? env('PUSHER_APP_KEY') : ''
+    'Pusher_WA' => env('PUSHER_APP_KEY') ? env('PUSHER_APP_KEY') : '',
+    'procesoName' => env('procesoName') ? env('procesoName') : ''
 ];
