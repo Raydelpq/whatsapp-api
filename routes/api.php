3785c05a5f85679a51ce251c0535e5c583fee3bf
Raydel Piloto Quesada <pilotoraydel@gmail.com>
2024-12-31 15:24:04 -0500

    Activar por APi

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Raydelpq\WhatsappApi\Http\Controllers\WhatsAppController;
+use Raydelpq\WhatsappApi\Http\Controllers\ConfigController;
 
 Route::post('/api/whatsapp',  [WhatsAppController::class, 'loadTaxistas'])->name('whatsapp_load');
 Route::post('/api/whatsapp/registro',  [WhatsAppController::class, 'registroTaxista'])->name('whatsapp_registro');
 Route::post('/api/expulsar',  [WhatsAppController::class, 'expulsarTaxistas'])->name('whatsapp_expulsar');
 Route::post('/api/getFondo',  [WhatsAppController::class, 'getFondo'])->name('whatsapp_getFondo');
+Route::post('/api/update-api-wa', [ConfigController::class, 'updateApiWa']);
+
---
+++ src/Http/Controllers/ConfigController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Raydelpq\WhatsappApi\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class ConfigController extends Controller
+{
+    public function updateApiWa(Request $request)
+    {
+        // Validar la solicitud
+        $request->validate([
+            'API_WA' => 'required|boolean',
+        ]);
+
+        // Obtener el nuevo valor
+        $newValue = $request->input('API_WA');
+
+        // Actualizar el valor en el archivo de configuración
+        Config::set('whatsappapi.API_WA', $newValue);
+
+        // Escribe la nueva configuración en el archivo .env
+        file_put_contents(app()->environmentFilePath(), str_replace(
+            'API_WA=' . env('API_WA'),
+            'API_WA=' . ($newValue ? 'true' : 'false'),
+            file_get_contents(app()->environmentFilePath())
+        ));
+
+
+        // Guardar el archivo de configuración (esto puede variar según cómo manejes tus configuraciones)
+        // Aquí podrías necesitar escribir manualmente el nuevo valor en el archivo .env o config.php
+
+        return response()->json(['message' => 'Configuración actualizada con éxito']);
+    }
+}
