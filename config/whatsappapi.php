d9845804d019f66e39d40dc6129b2fe57790084b
Raydel <pilotoraydel@gmail.com>
2025-04-06 16:40:24 -0400

    Update whatsappapi.php

--- config/whatsappapi.php
+++ config/whatsappapi.php
@@ -3,7 +3,8 @@
 return [
     'API_WA' => env('API_WA') ? env('API_WA') : false,
     'MIN_WA' => env('MIN_WA') ? env('MIN_WA') : 150,
-    'MIN_NOTIFICAR' => env('MIN_NOTIFICAR') ? env('MIN_WA') : 300,
+    'NOTIFICAR' => env('NOTIFICAR') ? env('NOTIFICAR') : false,
+    'MIN_NOTIFICAR' => env('MIN_NOTIFICAR') ? env('MIN_NOTIFICAR') : 300,
     'URL_WA' => env('URL_WA') ? env('URL_WA') : 'http://localhost',
     'PORT_WA' => env('PORT_WA') ? env('PORT_WA') : '99000',
     'ENDPOINT_WA' => env('ENDPOINT_WA') ? env('ENDPOINT_WA') : '/api/v1',
